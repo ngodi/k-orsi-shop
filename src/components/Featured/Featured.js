eec4f9b14f2fe627b05d8548cec8221275167a4a
ngodi <albertngodi@gmail.com>
2020-02-06 11:38:32 +0100

    featured component created

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -5,7 +5,11 @@ import './Featured.scss';
 const Featured = () => {
   return (
     <div className="featured">
-      
+      <div className="top-featured">
+          <span className="featured-title"><p>Our Featured Item</p></span>
+          <span className="featured-desc"></span>
+          <div className="featured-controls"></div>
+      </div>
     </div>
   );
 }
